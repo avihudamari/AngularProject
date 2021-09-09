f9e51234c7e80eaa0760b8d2bb8210cf7573eec0
avihu damari <tchvutsntrh@gmail.com>
2021-09-09 14:27:16 +0300

    g

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowAddServer = false;
+  addServerStatus = 'server was not created';
+  serverName = '';
 
-  constructor() { }
+  constructor() { 
+    setTimeout( ()=> {
+      this.allowAddServer = true;
+    },2000);
+  }
 
   ngOnInit(): void {
   }
+  onClickAddServer () {
+    this.addServerStatus = 'server created. server name is: ' + this.serverName;
+  }
 
+  onChangeServerName (e) {
+    this.serverName = e.target.value;
+  }
 }
