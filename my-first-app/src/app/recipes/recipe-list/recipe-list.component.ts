4e90d426a2712c5f0821f6f3f4d2da3c72a84f15
avihu damari <tchvutsntrh@gmail.com>
2021-09-12 17:07:09 +0300

    end Section 3: Course Project - The Basics

--- my-first-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ my-first-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('first recipe', 'desc of first recipe', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=768,574'),
+    new Recipe('second recipe', 'desc of second recipe', 'https://www.vegrecipesofindia.com/wp-content/uploads/2013/11/instant-pot-chana-masala-recipe-3-280x280.jpg')
+  ];
 
   constructor() { }
 
--- my-first-app/src/app/shopping-list/shopping-list.component.ts
+++ my-first-app/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.modal';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Tomato', 3),
+    new Ingredient('Cucamber', 5)
+  ];
+  
+  log(val) { console.log(val); }
 
   constructor() { }
 
